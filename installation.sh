1ea840724c792337bfdd71ef61af0cfaa927e4a6
juanjosegongora <gongoracontrerasjuanjose@gmail.com>
2020-03-21 13:01:08 +0100

    Ready

--- installation.sh
+++ installation.sh
@@ -6,7 +6,7 @@ apt update
 #INSTALAR DOCKER
 apt install docker.io -y
 
-# INSTALACION DE COMPONENTES DE CUBERNETES
+# INSTALACION DE COMPONENTES DE KUBERNETES
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 apt-get update
