bbc5c41584e6654863b1a21edaab65ab5e07d434
juanjosegongora <gongoracontrerasjuanjose@gmail.com>
2020-03-31 12:54:38 +0200

    Ready

--- installation.sh
+++ installation.sh
@@ -13,8 +13,8 @@ apt-get update
 apt install kubelet kubeadm kubectl -y
 
 # INSTALACION DE MINIKUBE.
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-install minikube-linux-amd64 /usr/local/bin/minikube
+# curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+# install minikube-linux-amd64 /usr/local/bin/minikube
 
 # HABILITAR EL SERVICIO DE DOCKER
 systemctl enable docker.service
